1bd27456eb4b1c2ef0a1bdffe842361fe42d62c0
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-05-15 19:32:39 +0000

    binding update

--- trunk/mono/gnomebulb/UIComposition/BindingResolver.cs
+++ trunk/mono/gnomebulb/UIComposition/BindingResolver.cs
@@ -7,8 +7,108 @@ using System.ComponentModel;
 
 namespace UIComposition
 {
+
 	static class BindingResolver
 	{
+        class PropertyToPropertyBindingDefinition
+        {
+
+            PropertyDescriptor targetProperty;
+            object target;
+            PropertyDescriptor sourceProperty;
+            object source;
+
+            string sourcePropertyName, targetPropertyName;
+
+            public PropertyToPropertyBindingDefinition(object source, string sourcePropertyName, object target, string targetPropertyName)
+            {
+                this.source = source;
+                this.sourcePropertyName = sourcePropertyName;
+                this.targetPropertyName = targetPropertyName;
+
+                sourceProperty = TypeDescriptor.GetProperties(source)[sourcePropertyName];
+
+                if (source is INotifyPropertyChanged)
+                {
+                    (source as INotifyPropertyChanged).PropertyChanged += new PropertyChangedEventHandler(SourcePropertyChanged);
+                }
+
+                this.target = target;
+                if (target is INotifyPropertyChanged)
+                {
+                    (target as INotifyPropertyChanged).PropertyChanged +=new PropertyChangedEventHandler(TargetPropertyChanged);
+                }
+                targetProperty = TypeDescriptor.GetProperties(target)[targetPropertyName];
+            }
+
+            void SourcePropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == sourcePropertyName)
+                {
+                    SourceToTarget();
+                }
+            }
+
+            void TargetPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == targetPropertyName)
+                {
+                    TargetToSource();
+                }
+            }
+
+            public void SourceToTarget()
+            {
+                object val = sourceProperty.GetValue(source);
+                try
+                {
+                    targetProperty.SetValue(target, val);
+                    if (target is Widget)
+                    {
+                        Gtk.Application.Invoke( 
+                            (o, e) => 
+                        (target as Widget).QueueDraw()
+                        );
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error updating binding: " + e.ToString());
+                }
+            }
+
+            public void TargetToSource()
+            {
+                object val = targetProperty.GetValue(source);
+                try
+                {
+                    sourceProperty.SetValue(target, val);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error updating binding: " + e.ToString());
+                }
+            }
+
+        }
+
+        static List<PropertyToPropertyBindingDefinition> currentBindings;
+
+        static BindingResolver()
+        {
+            currentBindings = new List<PropertyToPropertyBindingDefinition>();
+        }
+
+        public static void CreateBinding(this Widget target, string targetPropertyName, object source, string sourcePropertyName)
+        {
+            currentBindings.Add(new PropertyToPropertyBindingDefinition(source, sourcePropertyName, target, targetPropertyName));
+        }
+
+        public static void UpdateBinding(this Widget target, string targetPropertyName)
+        {
+
+        }
+
         public static void UpdateSourceBinding(this Widget target, object dataContext, object value, string binding)
         {
             Type type = dataContext.GetType();
@@ -22,13 +122,10 @@ namespace UIComposition
             {
                 Console.WriteLine(e.ToString());
             }
-
         }
 
         public static object GetBindingValue(this Widget target, object dataContext, string binding)
         {
-            Type type = dataContext.GetType();
-
             PropertyDescriptor descriptor = TypeDescriptor.GetProperties(dataContext)[binding];
             try
             {
@@ -39,7 +136,6 @@ namespace UIComposition
                 Console.WriteLine(e.ToString());
             }
             return null;
-
         }
 	}
 }
