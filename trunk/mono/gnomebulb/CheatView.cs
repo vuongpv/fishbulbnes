744735768a64acc8dfe5b8a374aab4863b74f092
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-05-15 17:45:04 +0000

    * GtkNes.pidb: * CheatView.cs: * FrontPanel.cs: * GtkNes.csproj: * VolumeWidget.cs: * gtk-gui/gui.stetic: * gtk-gui/MainWindow.cs: * gtk-gui/GtkNes.CheatView.cs: * gtk-gui/GtkNes.FrontPanel.cs: * gtk-gui/GtkNes.VolumeWidget.cs:

---
+++ trunk/mono/gnomebulb/CheatView.cs
@@ -0,0 +1,17 @@
+
+using System;
+
+namespace GtkNes
+{
+	
+	
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CheatView : Gtk.Bin
+	{
+		
+		public CheatView()
+		{
+			this.Build();
+		}
+	}
+}
---
+++ trunk/mono/gnomebulb/FrontPanel.cs
@@ -0,0 +1,17 @@
+
+using System;
+
+namespace GtkNes
+{
+	
+	
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class FrontPanel : Gtk.Bin
+	{
+		
+		public FrontPanel()
+		{
+			this.Build();
+		}
+	}
+}
