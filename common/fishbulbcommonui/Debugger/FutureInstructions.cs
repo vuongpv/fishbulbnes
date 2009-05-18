02beee3ce2407f78eb8c99ac5480b5c49fd4f0e9
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-05-18 17:21:30 +0000

    * Debugger: * fishbulbcommonui.csproj: * Debugger/FutureInstructions.cs:

---
+++ common/fishbulbcommonui/Debugger/FutureInstructions.cs
@@ -0,0 +1,75 @@
+using NES.CPU.nitenedo;
+using System;
+using System.Collections.Generic;
+
+using Fishbulb.Common.UI;
+
+namespace fishbulbcommonui
+{
+	
+	
+	public class FutureInstructions : IProfileViewModel
+	{
+
+		NESMachine machine;
+		public FutureInstructions(NESMachine machine)
+		{
+			this.machine = machine;
+			machine.DebugInfoChanged+= (o,a) => Console.WriteLine(a);
+		}
+		
+		#region IProfileViewModel implementation
+		public string CurrentView {
+			get {
+			 	return "AsmViewer";
+			}
+		}
+		
+		
+		Dictionary<string, ICommandWrapper> commands = new Dictionary<string, ICommandWrapper>();
+		
+		public Dictionary<string, ICommandWrapper> Commands {
+			get {
+				return commands;
+			}
+		}
+		
+		
+		public System.Collections.Generic.IEnumerable<IProfileViewModel> ChildViewModels {
+			get {
+				return new List<IProfileViewModel>();
+			}
+		}
+		
+		public string CurrentRegion {
+			get {
+			 return "debugger.0";
+			}
+		}
+		
+		public string Header {
+			get {
+				return "Future Instructions";
+			}
+		}
+		
+		public object DataModel {
+			get {
+				return machine.DebugInfo.FutureOps;
+			}
+		}
+
+		#endregion
+
+		#region INotifyPropertyChanged implementation
+		public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+		
+		public void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged!=null)
+				PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+		}
+		#endregion
+		
+	}
+}
