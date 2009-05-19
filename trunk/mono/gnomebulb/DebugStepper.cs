01dfad1a8f9b9adf9f2cf54a29f156e09388b8b5
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-05-19 00:02:34 +0000

    * GtkNes.pidb: * GtkNes.csproj: * DebugStepper.cs: * gtk-gui/gui.stetic: * gtk-gui/GtkNes.DebugStepper.cs:

---
+++ trunk/mono/gnomebulb/DebugStepper.cs
@@ -0,0 +1,17 @@
+
+using System;
+
+namespace GtkNes
+{
+	
+	
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DebugStepper : Gtk.Bin
+	{
+		
+		public DebugStepper()
+		{
+			this.Build();
+		}
+	}
+}
