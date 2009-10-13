83ba496d43045b72c3be9bc6bf9dcb498ddf96bc
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-10-13 12:03:30 +0000

    f

---
+++ common/fishbulbcore/Sound/SoundThread.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NES.CPU.nitenedo;
+using System.Threading;
+using System.ComponentModel;
+
+namespace NES.Sound
+{
+    public class SoundThreader : IDisposable
+    {
+        IWavStreamer _wavePlayer;
+
+        public IWavStreamer WavePlayer
+        {
+            get { return _wavePlayer; }
+            set { _wavePlayer = value; }
+        }
+
+        Thread myThread;
+        public SoundThreader(IWavStreamer streamer)
+        {
+
+            _wavePlayer = streamer;
+
+            ThreadPool.QueueUserWorkItem(PlaySound, null);
+
+        }
+
+        public void OnSoundStatusChanged(object sender, NES.CPU.Machine.BeepsBoops.SoundStatusChangeEventArgs e)
+        {
+            _wavePlayer.Muted = e.Muted;
+        }
+
+        public void PlaySound(object o)
+        {
+            _wavePlayer.PlayPCM();
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            _wavePlayer.Dispose();
+        }
+
+        #endregion
+    }
+}
