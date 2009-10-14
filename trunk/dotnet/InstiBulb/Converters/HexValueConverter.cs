a4d16e18b79053d81358618bf9442e914655205e
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-10-14 12:25:56 +0000

    instibulb new cheats

---
+++ trunk/dotnet/InstiBulb/Converters/HexValueConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace InstiBulb.Converters
+{
+    public class HexValueConverter : IValueConverter
+    {
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int? length = parameter as int?;
+
+            if (length == null)
+            {
+                length = 2;
+            }
+            // icky unboxing!
+            if (targetType == typeof(string))
+            {
+                return string.Format("{0:x" + length.ToString() + "}", value);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
