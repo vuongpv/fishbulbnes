5b168573ec3fdaf36d65abcdd2a45d657a4f48a0
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-10-24 15:01:09 +0000

    chr rom handling in cartridge (start)

---
+++ common/fishbulbcore/PixelWhizzlerClasses/PixelWhizzler.VidRAMHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NES.CPU.PPUClasses
+{
+    public partial class PixelWhizzler
+    {
+        private byte[] _vidRAM = new byte[0x4000];
+
+        public byte[] VidRAM
+        {
+            get { return _vidRAM; }
+            set { _vidRAM = value; }
+        }
+
+        public byte[] cartCopyVidRAM
+        {
+            get { return _vidRAM; }
+            set { _vidRAM = value; }
+        }
+    }
+}
