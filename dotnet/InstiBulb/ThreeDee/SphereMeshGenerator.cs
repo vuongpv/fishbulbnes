15e8b23b03185e78637eeeb366e60927afa87b26
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-10-27 19:12:53 +0000

    Trouble is in the bubble

--- dotnet/InstiBulb/ThreeDee/SphereMeshGenerator.cs
+++ dotnet/InstiBulb/ThreeDee/SphereMeshGenerator.cs
@@ -151,6 +151,98 @@ namespace InstiBulb.ThreeDee
 
         }
 
+        public void MakeBoundingSphere(List<Point3D> V)
+    {
+        Point3D C;                           // Center of ball
+        double rad, rad2;                   // radius and radius squared
+        double xmin, xmax, ymin, ymax, zmin, zmax;      // bounding box extremes
+        int   Pxmin, Pxmax, Pymin, Pymax, Pzmin, Pzmax;  // index of V[] at box extreme
+        int n = V.Count;
+        if (n == 0) return;
+        // find a large diameter to start with
+        // first get the bounding box and V[] extreme points for it
+        xmin = xmax = V[0].X;
+        ymin = ymax = V[0].Y;
+        zmin = zmax = V[0].Z;
+
+        Pxmin = Pxmax = Pymin = Pymax = Pzmax = Pzmin = 0;
+        for (int i=1; i<n; i++) {
+            if (V[i].X < xmin) {
+                xmin = V[i].X;
+                Pxmin = i;
+            }
+            else if (V[i].X > xmax) {
+                xmax = V[i].X;
+                Pxmax = i;
+            }
+            if (V[i].Y < ymin) {
+                ymin = V[i].Y;
+                Pymin = i;
+            }
+            else if (V[i].Y > ymax) {
+                ymax = V[i].Y;
+                Pymax = i;
+            }
+            if (V[i].Z < zmin)
+            {
+                zmin = V[i].Z;
+                Pzmin = i;
+            }
+            else if (V[i].Z > zmax)
+            {
+                zmax = V[i].Z;
+                Pzmax = i;
+            }
+
+        }
+        // select the largest extent as an initial diameter for the ball
+        Vector3D dVx = Point3D.Subtract(V[Pxmax], V[Pxmin]); // diff of Vy max and min
+        Vector3D dVy = Point3D.Subtract(V[Pymax], V[Pymin]); // diff of Vy max and min
+        Vector3D dVz = Point3D.Subtract(V[Pzmax], V[Pzmin]); // diff of Vy max and min
+
+        double dx2 = dVx.LengthSquared;// (dVx); // Vx diff squared
+        double dy2 = dVy.LengthSquared; // Vy diff squared
+        double dz2 = dVz.LengthSquared;
+
+
+        if (dx2 >= dy2 && dx2 >= dz2) {                     // x direction is largest extent
+            C = V[Pxmin] + (dVx / 2.0);         // Center = midpoint of extremes
+            rad2 = (V[Pxmax] - C).LengthSquared;         // radius squared
+        }
+        else if (dy2 >= dx2 && dy2 >= dz2)
+        {                                // y direction is largest extent
+            C = V[Pymin] + (dVy / 2.0);         // Center = midpoint of extremes
+            rad2 = (V[Pymax] - C).LengthSquared;         // radius squared
+        }
+        else
+        {
+            C = V[Pzmin] + (dVz / 2.0);         // z direction is largest
+            rad2 = (V[Pzmax] - C).LengthSquared;         // radius squared
+        }
+        rad = Math.Sqrt(rad2);
+
+        // now check that all points V[i] are in the ball
+        // and if not, expand the ball just enough to include them
+        Vector3D dV;
+        double dist, dist2;
+        for (int i=0; i<n; i++) {
+            dV = V[i] - C;
+            dist2 = dV.LengthSquared;
+            if (dist2 <= rad2)    // V[i] is inside the ball already
+                continue;
+            // V[i] not in ball, so expand ball to include it
+            dist = Math.Sqrt(dist2);
+            rad = (rad + dist) / 2.0;         // enlarge radius just enough
+            rad2 = rad * rad;
+            C = C + ((dist-rad)/dist) * dV;   // shift Center toward V[i]
+        }
+        this.Center = C;
+        this.Radius = rad;
+        return;
+    }
+
+
+
     }
 
 
