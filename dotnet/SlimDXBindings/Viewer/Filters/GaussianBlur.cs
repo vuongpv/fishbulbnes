e5978df465bd59d5295c4dc7ca526d10806cb231
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-10-30 20:48:15 +0000

    working on a fullscreen quad abstraction for a more robust post-processing chain

---
+++ dotnet/SlimDXBindings/Viewer/Filters/GaussianBlur.cs
@@ -0,0 +1,127 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using SlimDX;
+//using SlimDX.Direct3D9;
+//using System.Reflection;
+
+//namespace SlimDXBindings.Viewer.Filters
+//{
+//    public class GaussianBlur
+//    {
+//        Device device;
+
+//        private Effect Effect = null;
+
+//        float[] Parameter_Offsets = null;
+//        float[] Parameter_Weights = null;
+//        int Parameter_ShaderIndex = 0;
+
+//        private float _StandardDeviation = 0.8f;
+//        private float _Amplitude = 0.4f;
+//        private bool _Horizontal = true;
+
+//        private Vector2 TargetDimensions = new Vector2();
+
+//        private bool _DirtyWeights = true;
+//        private float[] Weights = new float[9];
+//        private float[] Offsets = new float[9];
+
+//        public bool Horizontal
+//        {
+//            get
+//            {
+//                return _Horizontal;
+//            }
+//            set
+//            {
+//                _Horizontal = value;
+//                Parameter_ShaderIndex = (_Horizontal ? 0 : 1);
+                
+//            }
+//        }
+
+//        public float StandardDeviation
+//        {
+//            get
+//            {
+//                return _StandardDeviation;
+//            }
+//            set
+//            {
+//                if (_StandardDeviation != value)
+//                {
+//                    _DirtyWeights = true;
+//                }
+//                _StandardDeviation = value;
+//            }
+//        }
+
+//        public float Amplitude
+//        {
+//            get
+//            {
+//                return _Amplitude;
+//            }
+//            set
+//            {
+//                if (_Amplitude != value)
+//                {
+//                    _DirtyWeights = true;
+//                }
+
+//                _Amplitude = value;
+
+//            }
+//        }
+
+//    public GaussianBlur(Device device)
+//    {
+//        Effect = Effect.FromStream(device,
+//                Assembly.GetExecutingAssembly().GetManifestResourceStream("SlimDXBindings.Viewer.Filters.GaussianBlur.fx"),
+//                ShaderFlags.None);
+
+//        Parameter_SourceTexture = Effect.Parameters["SourceTexture"];
+
+//        Parameter_Offsets = Effect.Parameters["Offsets"];
+//        Parameter_Weights = Effect.Parameters["Weights"];
+//        Parameter_ShaderIndex = Effect.Parameters["ShaderIndex"];
+//    }
+
+//    public override void PostProcess(Texture sourceTexture,
+//        RenderToSurface result)
+//    {
+//        Parameter_SourceTexture.SetValue(sourceTexture);
+
+//        if (_DirtyWeights)
+//        {
+//            if (_Horizontal)
+//            {
+                
+//                Gaussian.Fill(Offsets, 1.0f / TargetDimensions.X,
+//                Weights, 0, _StandardDeviation, _Amplitude);
+//            }
+//            else
+//            {
+//                Gaussian.Fill(Offsets, 1.0f / TargetDimensions.Y,
+//                Weights, 0, _StandardDeviation, _Amplitude);
+//            }
+
+//            Parameter_Weights.SetValue(Weights);
+//            Parameter_Offsets.SetValue(Offsets);
+
+//            _DirtyWeights = false;
+//        }
+
+
+
+//        device.SetRenderTarget(0, surf);
+//        _GraphicsDevice.Clear(Color.Black);
+
+//        DrawQuad(Effect);
+
+//        _GraphicsDevice.SetRenderTarget(0, null);
+//    }
+
+//}
---
+++ dotnet/SlimDXBindings/Viewer/Filters/QuadRenderer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlimDX.Direct3D9;
+using System.Runtime.InteropServices;
+
+namespace SlimDXBindings.Viewer.Filters
+{
+    public class QuadRenderer  : IDisposable
+    {
+        readonly Device device;
+        VertexDeclaration vd;
+        SlimDX.Vector2 v1; SlimDX.Vector2 v2;
+        VertexPositionTexture[] verts = BasicQuad;
+
+
+        public QuadRenderer(Device device, SlimDX.Vector2 v1, SlimDX.Vector2 v2)
+        {
+            this.device = device;
+            vd = new VertexDeclaration(device, vertexDecl);
+            this.v1 = v1;
+            this.v2 = v2;
+
+            verts[0].Position.X = v2.X; //-1
+            verts[0].Position.Y = v1.Y; //1
+
+            verts[1].Position.X = v1.X; //1
+            verts[1].Position.Y = v1.Y; //1
+
+            verts[2].Position.X = v1.X; //1
+            verts[2].Position.Y = v2.Y; //-1
+
+            verts[3].Position.X = v2.X; //-1
+            verts[3].Position.Y = v2.Y; //-1
+
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct VertexPositionTexture
+        {
+            public SlimDX.Vector4 Position;
+            public SlimDX.Vector2 Texture;
+
+            public VertexPositionTexture(SlimDX.Vector4 Position, SlimDX.Vector2 Texture)
+            {
+                this.Position = Position;
+                this.Texture = Texture;
+            }
+        }
+
+        static VertexElement[] vertexDecl = new VertexElement[]
+            {
+                new VertexElement(0, 0, DeclarationType.Float4, DeclarationMethod.Default, DeclarationUsage.PositionTransformed, 0),
+                new VertexElement(0, 16, DeclarationType.Float2, DeclarationMethod.Default, DeclarationUsage.TextureCoordinate, 0),
+                VertexElement.VertexDeclarationEnd
+            };
+
+        static VertexPositionTexture[] BasicQuad = new VertexPositionTexture[]
+            {
+                new VertexPositionTexture(
+                    new SlimDX.Vector4(0,0,1,0),
+                    new SlimDX.Vector2(1,1)),
+                new VertexPositionTexture(
+                    new SlimDX.Vector4(0,0,1,0),
+                    new SlimDX.Vector2(0,1)),
+                new VertexPositionTexture(
+                    new SlimDX.Vector4(0,0,1,0),
+                    new SlimDX.Vector2(0,0)),
+                new VertexPositionTexture(
+                    new SlimDX.Vector4(0,0,1,0),
+                    new SlimDX.Vector2(1,0))
+            };
+
+        static short[] ib = new short[] { 0, 1, 2, 2, 3, 0 };
+
+        public void Draw()
+        {
+            device.VertexFormat = (VertexFormat.PositionW | VertexFormat.Texture0 );
+            device.VertexDeclaration = vd;
+
+            device.DrawIndexedUserPrimitives<short, VertexPositionTexture>
+                (PrimitiveType.TriangleList, 0, 4, 2, ib, Format.Index16, verts, Marshal.SizeOf(typeof(VertexPositionTexture)));
+
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            vd.Dispose();
+        }
+
+        #endregion
+    }
+
+
+}
