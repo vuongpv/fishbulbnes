4c522dc1c7eef31218b1dbd77796adcbfd4f4639
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-11-17 17:56:25 +0000

    embeddable wpf content is almost a first rate feature

---
+++ dotnet/SlimDXBindings/Viewer10/Helpers/MouseEventMapper.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SlimDXBindings.Viewer10.Filter;
+
+namespace SlimDXBindings.Viewer10.Helpers
+{
+
+    public enum FakedEventTypes
+    {
+        MOUSECLICK,
+        MOUSEDOUBLECLICK,
+        MOUSEDOWN,
+        MOUSEUP,
+        MOUSEMOVE,
+        MOUSEENTER,
+        MOUSELEAVE,
+        KEYPRESS
+    }
+
+    public class FakeEvent
+    {
+        public FakedEventTypes EventType
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// For mouse events, 1 = left mouse button, 2 = right mouse button, 4 = middle mouse button, 8 = extended 1, 16 = extended 2
+        /// For keyboard events, the int corresponding to the key event based Wpf Keys enum
+        /// </summary>
+        public int EventData
+        {
+            get;
+            set;
+        }
+
+        public double X
+        {
+            get;
+            set;
+        }
+
+        public double Y
+        {
+            get;
+            set;
+        }
+    }
+
+    public class FakeEventMapper
+    {
+
+        Form form;
+        FilterChain chain;
+        public FakeEventMapper(Form form, FilterChain chain)
+        {
+            this.form = form;
+            this.chain = chain;
+            form.MouseClick += new MouseEventHandler(form_MouseClick);
+            form.MouseDoubleClick += new MouseEventHandler(form_MouseDoubleClick);
+            form.MouseMove += new MouseEventHandler(form_MouseMove);
+            form.MouseDown += new MouseEventHandler(form_MouseDown);
+            form.MouseUp += new MouseEventHandler(form_MouseUp);
+        }
+
+        void form_MouseUp(object sender, MouseEventArgs e)
+        {
+            var p = new FakeEvent()
+            {
+                EventType = FakedEventTypes.MOUSEDOWN,
+                X = ((double)e.X) / ((double)form.Width),
+                Y = ((double)e.Y) / ((double)form.Height),
+            };
+            chain.ProcessEvent(p);
+        }
+
+        void form_MouseDown(object sender, MouseEventArgs e)
+        {
+            var p = new FakeEvent()
+            {
+                EventType = FakedEventTypes.MOUSEDOWN,
+                X = ((double)e.X) / ((double)form.Width),
+                Y = ((double)e.Y) / ((double)form.Height),
+            };
+            chain.ProcessEvent(p);
+        }
+
+        void form_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            var p = new FakeEvent()
+            {
+                EventType = FakedEventTypes.MOUSEMOVE,
+                X = ((double)e.X) / ((double)form.Width),
+                Y = ((double)e.Y) / ((double)form.Height),
+            };
+            chain.ProcessEvent(p);
+        }
+
+        //void form_MouseLeave(object sender, EventArgs e)
+        //{
+        //    var p = new FakeEvent()
+        //    {
+        //        EventType = FakedEventTypes.MOUSELEAVE,
+        //        X = 0,
+        //        Y = 0,
+        //    };
+        //    chain.ProcessEvent(p);
+        //}
+
+        //void form_MouseEnter(object sender, EventArgs e)
+        //{
+        //    var p = new FakeEvent()
+        //    {
+        //        EventType = FakedEventTypes.MOUSEENTER,
+        //        X = 0,
+        //        Y = 0,
+        //    };
+        //    chain.ProcessEvent(p);
+        //}
+
+        void form_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var p = new FakeEvent()
+            {
+                EventType = FakedEventTypes.MOUSEDOUBLECLICK,
+                X = ((double)e.X) / ((double)form.Width),
+                Y = ((double)e.Y) / ((double)form.Height),
+            };
+            chain.ProcessEvent(p);
+        }
+
+        void form_MouseClick(object sender, MouseEventArgs e)
+        {
+            var p = new FakeEvent()
+            {
+                EventType = FakedEventTypes.MOUSECLICK,
+                X = ((double)e.X) / ((double)form.Width),
+                Y = ((double)e.Y) / ((double)form.Height),
+            };
+            chain.ProcessEvent(p);
+        }
+    }
+}
