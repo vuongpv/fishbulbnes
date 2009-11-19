94a9858c2549b21a7761308e3ca3372914894135
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-11-19 21:56:09 +0000

    messin with zapper

---
+++ trunk/dotnet/InstibulbWpfUI/ControlPanel/ControlPanelView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using InstibulbWpfUI;
+
+namespace InstibulbWpfUI
+{
+    /// <summary>
+    /// Interaction logic for ControlPanelView.xaml
+    /// </summary>
+    public partial class ControlPanelView : EmbeddableUserControl
+    {
+
+
+
+        public ControlPanelView()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler<EventArgs> UpdateKeyhandlingEvent;
+
+
+
+        public bool SuppressKeystrokes
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ trunk/dotnet/InstibulbWpfUI/ControlPanel/IAm3DEmbeddable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace SlimDXBindings.Viewer10.ControlPanel
+{
+    public interface IAm3DEmbeddable
+    {
+        bool NeedsRefreshing { get; }
+        void HandleKeystroke(Key key);
+
+    }
+}
