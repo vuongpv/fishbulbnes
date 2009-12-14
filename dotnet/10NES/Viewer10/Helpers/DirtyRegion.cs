fa21f89ba71b8ad4fde78eea880bbc57812869fc
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2009-12-14 04:18:20 +0000

    huge.. lots of changes, 10NES ho

---
+++ dotnet/10NES/Viewer10/Helpers/DirtyRegion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace _10NES.Viewer10.Helpers
+{
+    public class DirtyRegion
+    {
+        public DateTime DirtyAt
+        {
+            get;
+            set;
+        }
+
+        public Rectangle DirtyArea
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
