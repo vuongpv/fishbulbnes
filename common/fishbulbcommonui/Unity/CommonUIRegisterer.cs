2b651c2d035ef45db50b7a2ff4b213aec084c21a
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2010-01-13 05:36:13 +0000

    unity checkin

---
+++ common/fishbulbcommonui/Unity/CommonUIRegisterer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+using Fishbulb.Common.UI;
+using GtkNes;
+
+namespace fishbulbcommonui.Unity
+{
+    public static class CommonUIRegisterer
+    {
+        public static IUnityContainer RegisterCommonUITypes(this IUnityContainer container)
+        {
+            container.RegisterType<ControlPanelVM>();
+            container.RegisterType<SoundViewModel>();
+            container.RegisterType<IViewModel, SoundViewModel>("SoundVM", new ContainerControlledLifetimeManager());
+            container.RegisterType<IViewModel, ControlPanelVM>("ControlPanel", new ContainerControlledLifetimeManager());
+
+            return container;
+        }
+    }
+}
