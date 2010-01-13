52f1f2a862b6a6765ae316a6ce111a6e37b40ba4
jhartrick@comcast.net <jhartrick@comcast.net@f80d8a06-40df-11de-952f-710615d9d0d5>
2010-01-13 05:33:25 +0000

    add?

---
+++ trunk/dotnet/InstibulbWpfUI/ControlPanel/CheatControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Fishbulb.Common.UI;
+
+namespace InstiBulb
+{
+	/// <summary>
+	/// Interaction logic for CheatControl.xaml
+	/// </summary>
+	public partial class CheatControl
+	{
+		public CheatControl()
+		{
+			this.InitializeComponent();
+		}
+
+
+	}
+}
