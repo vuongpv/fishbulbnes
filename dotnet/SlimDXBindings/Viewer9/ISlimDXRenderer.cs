32a50f377577ab56bb27d278d533eaf2aa6e4518
joe.hartrick@gmail.com <joe.hartrick@gmail.com@f80d8a06-40df-11de-952f-710615d9d0d5>
2010-02-22 00:35:07 +0000

    Viewer9 fix

---
+++ dotnet/SlimDXBindings/Viewer9/ISlimDXRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NES.CPU.nitenedo.Interaction;
+namespace SlimDXBindings.Viewer
+{
+    public interface ISlimDXRenderer : IDisposable
+    {
+        void InitializeScene();
+        void Render();
+
+        NESPixelFormats PixelFormat { get; }
+    }
+}
