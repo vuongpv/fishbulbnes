c1672bafef5e946ba6b782be836bf6075048d1ab
joe.hartrick@gmail.com <joe.hartrick@gmail.com@f80d8a06-40df-11de-952f-710615d9d0d5>
2010-02-24 03:52:17 +0000

    removing old viewer and viewer10 directories (renamed viewer9 viewerX)

--- trunk/dotnet/SlimDXBindings/Properties/AssemblyInfo.cs
+++ trunk/dotnet/SlimDXBindings/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SlimDXBindings")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SlimDX Bindings for FishBulb")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SlimDXBindings")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyCopyright("Copyright © Joe Hartrick")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
